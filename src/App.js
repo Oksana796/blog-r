0ab6d5047a87560bcd2c9285fcab96c30bf3de11
Oksana <ksenia79460@gmail.com>
2023-04-03 16:59:47 +0100

    navbar on top

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Topbar from "./topbar/Topbar.jsx";
+
 function App() {
-  return <div className="App">Blog</div>;
+  return (
+    <div className="App">
+      <Topbar />
+    </div>
+  );
 }
 
 export default App;
