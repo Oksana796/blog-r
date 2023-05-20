9ce8bf71ee9d3c8d76680b528641ffad431035a1
Oksana <ksenia79460@gmail.com>
2023-05-20 14:52:46 +0100

    created and styled register page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Topbar from "./components/topbar/Topbar.jsx";
 //import Write from "./pages/write/Write.jsx";
 //import Home from "./pages/home/Home.jsx";
 //import Settings from "./pages/settings/Settings.jsx";
-import Login from "./pages/login/Login.jsx";
+//import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
 
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Login />
+      <Register />
     </div>
   );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,25 @@
+import "./register.css";
+
+const Register = () => {
+  return (
+    <div className="register">
+     <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input type="text" placeholder="Username" className="registerInput" />
+        <label>Email</label>
+        <input type="text" placeholder="Email" className="registerInput" />
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          className="registerInput"
+        />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+};
+
+export default Register;
